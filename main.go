ce0143abb0e927ba5ef5800dda171df72f3de4c8
hyprhex <swapapis@gmail.com>
2024-05-28 10:02:01 +0100

    Add the handler we need to view and create short link

--- main.go
+++ main.go
@@ -9,9 +9,19 @@ func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Welcome to shortify"))
 }
 
+func linkView(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("view list of links"))
+}
+
+func linkCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Generate a short link"))
+}
+
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/link/view", linkView)
+	mux.HandleFunc("/link/create", linkCreate)
 
 	log.Printf("Starting on server 4000")
 	err := http.ListenAndServe(":4000", mux)
